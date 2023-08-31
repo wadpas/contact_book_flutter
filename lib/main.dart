e1351c0dde63120799105d1945c58cefefe5576a
wadpas <wadpas@gmail.com>
2023-08-31 12:12:15 +0300

    added new contact screen

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,9 @@ void main() {
         useMaterial3: true,
       ),
       home: const MyHomePage(),
+      routes: {
+        '/new-contact': (context) => const NewContactView(),
+      },
     ),
   );
 }
@@ -48,12 +51,74 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final contactBook = ContactBook();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Contacts'),
       ),
-      body: const Center(
-        child: Text('Home'),
+      body: ListView.builder(
+        itemCount: contactBook.length,
+        itemBuilder: (context, index) {
+          final contact = contactBook.contact(atIndex: index)!;
+          return ListTile(
+            title: Text(contact.name),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () async {
+          await Navigator.of(context).pushNamed('/new-contact');
+        },
+      ),
+    );
+  }
+}
+
+class NewContactView extends StatefulWidget {
+  const NewContactView({super.key});
+
+  @override
+  State<NewContactView> createState() => _NewContactViewState();
+}
+
+class _NewContactViewState extends State<NewContactView> {
+  late final TextEditingController _controller;
+
+  @override
+  void initState() {
+    _controller = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('New Contact'),
+      ),
+      body: Column(
+        children: [
+          TextField(
+            controller: _controller,
+            decoration: const InputDecoration(
+                hintText: 'Enter a new contact name here'),
+          ),
+          TextButton(
+            onPressed: () {
+              final contact = Contact(name: _controller.text);
+              ContactBook().add(contact: contact);
+              Navigator.of(context).pop();
+            },
+            child: const Text('Add contact'),
+          )
+        ],
       ),
     );
   }
